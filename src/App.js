d46e0d2de33c9f2581cab9f15c4984750f190267
aefehr <fehr.allie@gmail.com>
2025-04-06 15:30:01 -0400

    Create header component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@
 import React from 'react';
 import SpellPage from './pages/SpellPage';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return <SpellPage />;
+  return (
+    <div className="App">
+      <Header />
+      <SpellPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+// src/components/Header.jsx
+import React from 'react';
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+
+const Header = ({ onHomeClick, onSettingsClick }) => {
+  return (
+    <div className="header-container">
+      <div className="header-button-wrapper">
+        <div className="header-shadow" />
+        <button className="header-button" onClick={onHomeClick}>
+          <FontAwesomeIcon icon={faHome} className="header-icon" />
+          <span className="header-text">Home</span>
+        </button>
+      </div>
+
+      <div className="header-button-wrapper">
+        <div className="header-shadow" />
+        <button className="header-button" onClick={onSettingsClick}>
+          <span className="header-text">Text Size & Contrast</span>
+          <FontAwesomeIcon icon={faAngleDown} className="header-icon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+
