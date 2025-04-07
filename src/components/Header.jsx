afe1360c49d2a4601758fd2d92046efff78e32dd
aefehr <fehr.allie@gmail.com>
2025-04-07 15:27:43 -0400

    Add header underline

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,21 +6,23 @@ import { faHome, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 const Header = ({ onHomeClick, onSettingsClick }) => {
   return (
-    <div className="header-container">
-      <div className="header-button-wrapper">
-        <div className="header-shadow" />
-        <button className="header-button" onClick={onHomeClick}>
-          <FontAwesomeIcon icon={faHome} className="header-icon" />
-          <span className="header-text">Home</span>
-        </button>
-      </div>
+    <div className="header-wrapper">
+      <div className="header-container">
+        <div className="header-button-wrapper">
+          <div className="header-shadow" />
+          <button className="header-button" onClick={onHomeClick}>
+            <FontAwesomeIcon icon={faHome} className="header-icon" />
+            <span className="header-text">Home</span>
+          </button>
+        </div>
 
-      <div className="header-button-wrapper">
-        <div className="header-shadow" />
-        <button className="header-button" onClick={onSettingsClick}>
-          <span className="header-text">Text Size & Contrast</span>
-          <FontAwesomeIcon icon={faAngleDown} className="header-icon" />
-        </button>
+        <div className="header-button-wrapper">
+          <div className="header-shadow" />
+          <button className="header-button" onClick={onSettingsClick}>
+            <span className="header-text">Text Size & Contrast</span>
+            <FontAwesomeIcon icon={faAngleDown} className="header-icon" />
+          </button>
+        </div>
       </div>
     </div>
   );
