098d47d39f807f0ceb816c0b3b7e3c34d73bccf8
aefehr <fehr.allie@gmail.com>
2025-04-09 12:41:09 -0400

    Add logo to header bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,22 @@
 import React from 'react';
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import logo from '../assets/logos/logo.png';
 
 const Header = ({ onHomeClick, onSettingsClick }) => {
   return (
     <div className="header-wrapper">
       <div className="header-container">
-        <div className="header-button-wrapper">
-          <div className="header-shadow" />
-          <button className="header-button" onClick={onHomeClick}>
-            <FontAwesomeIcon icon={faHome} className="header-icon" />
-            <span className="header-text">Home</span>
-          </button>
+        <div className="header-left">
+          <img src={logo} alt="App Logo" className="header-logo" />
+          <div className="header-button-wrapper">
+            <div className="header-shadow" />
+            <button className="header-button" onClick={onHomeClick}>
+              <FontAwesomeIcon icon={faBars} className="header-icon" />
+              <span className="header-text">Menu</span>
+            </button>
+          </div>
         </div>
 
         <div className="header-button-wrapper">
