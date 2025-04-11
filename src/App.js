9e1cf3348d382ead0137b3b351ddae6ab7c8c2c7
Sydney Simon <sydsimon@sas.upenn.edu>
2025-04-11 00:20:31 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 // src/App.js
 import React from 'react';
 import SpellPage from './pages/SpellPage';
+import IncorrectSpellingPage from './pages/IncorrectPage';
+import CorrectPage from './pages/CorrectPage';
 import './App.css';
 import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CorrectSpellingPage from './pages/CorrectedSpellingPage';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SpellPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<SpellPage />} />
+          <Route path="/SpellPage" element={<SpellPage />} />
+          <Route path="/IncorrectSpellingPage" element={<IncorrectSpellingPage correctWord={''} userAnswer={''}/>} />
+          <Route path="/CorrectSpellingPage" element={<CorrectSpellingPage correctWord={''}/>} />
+          <Route path="/CorrectPage" element={<CorrectPage correctWord={''}/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
