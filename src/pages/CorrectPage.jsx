336cb6e11ba6d8abd1c95a19729253cd7e1bd3b6
aefehr <fehr.allie@gmail.com>
2025-04-13 11:18:20 -0400

    Fox correct page styling

--- src/pages/CorrectPage.jsx
+++ src/pages/CorrectPage.jsx
@@ -7,15 +7,15 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
 const CorrectPage = ({ correctWord, onNextWord }) => {
   return (
     <div className="App">
-      <header className="Spell-header" style={{ marginTop: 100, padding: 0, color: "white"}}>
+      <header className="Spell-header" style={{ marginTop: 100, padding: 0}}>
         <p >Correct!</p>
       </header>
-      <FontAwesomeIcon icon={faCheck} size="10x" style={{ color: "#ffffff"}} />
-      <header className="Spell-header" style={{color: "white"}}>
+      <FontAwesomeIcon icon={faCheck} size="10x" />
+      <header className="Spell-header">
        <p> {correctWord.toUpperCase()} </p>
       </header>
       <div>
-        <div className="button-wrapper submit-button-shadow">
+        <div className="button-wrapper">
           <div className="button-shadow" />
           <button className="action-button" style={{ margin: '20px' }} onClick={onNextWord}>
             Next Word
