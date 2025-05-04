b39789c89140280ebadc23dde1f808f25c7836af
Sydney Simon <sydsimon@sas.upenn.edu>
2025-05-04 16:06:31 -0400

    update corrected spelling bytton style

--- src/pages/CorrectedSpellingPage.jsx
+++ src/pages/CorrectedSpellingPage.jsx
@@ -12,12 +12,13 @@ const CorrectSpellingPage = ({ correctWord, onNextWord }) => {
         <p>{correctWord.toUpperCase().split('').join(' ')}</p>
       </header>
       <div>
-      <div className="button-wrapper">
-        <div className="button-shadow" />
-          <button className="action-button" style={{ margin: '90px' }} onClick={onNextWord}>
+
+      <div className="button-wrapper secondary-button-shadow">
+          <div className="button-shadow" />
+          <button className="action-button" onClick={onNextWord}>
           Next Word
           </button>
-        </div>
+          </div>
       </div>
     </div>
   );
